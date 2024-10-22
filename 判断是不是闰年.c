f1ccfe67aebe1aa3ba0bcac2f1ed0716053b5b94
nania8888 <1809740522@qq.com>
2024-10-22 21:05:50 +0800

    first test

---
+++ 判断是不是闰年.c
@@ -0,0 +1,12 @@
+//#include<stdio.h>
+//int main()
+//{
+//    int a;
+//    printf("请输入您要判断的年份：");
+//    scanf("%d",&a);
+//    if((a%400==0)||(a%4==0&&a%100!=0))
+//        printf("%d是闰年。",a);
+//    else
+//        printf("%d是平年。",a);
+//    return 0;
+//}
