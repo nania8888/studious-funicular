80c0695bbf82c6d76540dc28d39afd48b4f5684e
nania8888 <1809740522@qq.com>
2024-10-22 21:23:29 +0800

    测试是否成功

---
+++ 2个正整数和差积商.c
@@ -0,0 +1,23 @@
+//#include<stdio.h>
+//int main()
+//{
+//    int a,b;
+//    printf("请输入两个正整数：");
+//    scanf("%d%d",&a,&b);
+//    printf("这两个数的和是：%d\n",a+b);
+//    printf("这两个数的差是：%d\n",a-b);
+//    printf("这两个数的积是：%d\n",a*b);
+//    printf("这两个数的商是：%d\m",a/b);
+//    return 0;
+//}
+////#include<stdio.h>
+////int main()
+////{
+////    int m,n;
+////    scanf("%d%d",&m,&n);
+////    printf("%d+%d=%d\n",m,n,m+n);
+////    printf("%d-%d=%d\n",m,n,m-n);
+////    printf("%d*%d=%d\n",m,n,m-n);
+////    printf("%d/%d=%d\n",m,n,m/n);
+////    return 0;
+////}
