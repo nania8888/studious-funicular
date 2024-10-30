9fb85fd3df91aa50cd424b72b1a1c88f96fcd89e
nania8888 <1809740522@qq.com>
2024-10-30 08:12:51 +0800

    Add files via upload

---
+++ 9.华氏温度和摄氏度转换.c
@@ -0,0 +1,16 @@
+//#include<stdio.h>
+//int main(){
+//    float F=150;
+//    float C=5*(F-32)/9;
+//    printf("华氏温度150度对应的摄氏温度是：%.2f\n",C);
+////      浮点型
+//    return 0;
+//}
+//#include<stdio.h>
+//int main(){
+//    int F=150;
+//    int C=5*(F-32)/9;
+//    printf("华氏温度150度对应的摄氏温度是：%.2f\n",C);
+////    整型
+//    return 0;
+//}
